30189cac455093aec1e7ad11458ff57fba3f8e00
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2024-05-22 00:24:46 +0600

    electron react

---
+++ src/preload/index.js
@@ -0,0 +1,12 @@
+import { contextBridge } from 'electron'
+if (!process.contextIsolated) {
+  throw new Error('ContextIsolation must be enabled in the BrowserWindow')
+}
+
+try {
+  contextBridge.exposeInMainWorld('context', {
+    // TODO:
+  })
+} catch (error) {
+  console.log(error)
+}
---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+import { LoginForm } from './components/Auth/login'
+
+function App() {
+  return (
+    <div className="flex h items-center justify-center   ">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
